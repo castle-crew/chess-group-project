98d9aede5b13396670fdf82393b75a30dfb8d0ac
Kresmlin <kycmmas@gmail.com>
2019-04-27 17:55:38 +0000

    Adding Devise

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'devise'
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'castle-crew-chess.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
