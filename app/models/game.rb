c930e7e067d0b7989ae2df1a610b51d6e6ba93c1
Kresmlin <kycmmas@gmail.com>
2019-05-06 05:49:55 +0000

    Add available scope to games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,3 @@
 class Game < ApplicationRecord
+   scope :available, -> { where('(white_player IS NULL AND black_player IS NOT NULL) OR (black_player IS NULL AND white_player IS NOT NULL)') }
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,3 @@
 class Player < ApplicationRecord
+  has_many :games
 end
