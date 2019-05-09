1880853accba5506245d43aeed51ae9c4858f8d6
ZoeBKramer <zoebkramer@gmail.com>
2019-05-09 03:19:48 +0000

    added the valid move method to the king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,12 @@
 class King < Piece
+
+  def valid_move?(x, y)
+    if ( x_space + 1 == x or x_space - 1 == x or x == x_space ) && 
+       ( y_space + 1 == y or y_space - 1 == y or y == y_space ) &&
+       return true
+    end
+    
+    return false
+  end
+
 end
