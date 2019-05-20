ff8f783a29e27a2591d8d4cf5e3eabb74b34d53f
Goldukulele <kcramos1211@gmail.com>
2019-05-20 00:01:32 +0000

    pulling master from github

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,3 +1,22 @@
 class Pawn < Piece
 
+<<<<<<< HEAD
+=======
+  def valid_move?(x, y)
+    if x != x_space
+      return false
+    end
+    if y_space + 1 = y 
+      return true
+    end
+    if y_space + 2 = y 
+      if (y_space == 1 && color == "black") or (y_space == 6 && color == "white") 
+        return true
+      end
+    end
+
+    return false
+  end
+
+>>>>>>> 4839d5c3ea1ef9d170443bbf22da7b55b34fce71
 end
