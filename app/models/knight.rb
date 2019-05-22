837da9bfe9bfce517d69d3917f1cd7f4656afb3d
Goldukulele <kcramos1211@gmail.com>
2019-05-22 07:27:44 +0000

    Valid move for Knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,18 @@
 class Knight < Piece
+
+  def valid_move?(x, y)
+    if (x_space + 1 == x && y_space + 2 == y) || 
+       (x_space - 1 == x && y_space + 2 == y) ||
+       (x_space + 1 == x && y_space - 2 == y) ||
+       (x_space - 1 == x && y_space - 2 == y) ||
+       (y_space + 1 == y && x_space + 2 == x) ||
+       (y_space - 1 == y && x_space + 2 == x) ||
+       (y_space + 1 == y && x_space - 2 == x) ||
+       (y_space - 1 == y && x_space - 2 == x)
+      return true
+
+    else
+      return false
+    end
+  end
 end
